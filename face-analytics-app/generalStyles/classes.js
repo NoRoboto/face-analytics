4fdacbeb2583a1f3045ea74195d5c783fda14460
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-06-23 18:07:28 -0500

    Added general styles file

---
+++ face-analytics-app/generalStyles/classes.js
@@ -0,0 +1,16 @@
+export default {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  sectionText: {
+    color: '#FFFFFF',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+}
---
+++ face-analytics-app/generalStyles/colors.js
@@ -0,0 +1,5 @@
+export default {
+  primaryColor: "#2ECC71",
+  secondaryColor: "#3498DB",
+  tertiaryColor: "#9B59B6"
+}
