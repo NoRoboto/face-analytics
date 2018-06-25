8abd3e2c7ecc6b1c86399376fdac25baed9ef088
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-06-25 16:24:44 -0500

    WIP

--- face-analytics-app/components/camera/SnapShot.js
+++ face-analytics-app/components/camera/SnapShot.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import {
   Text,
-  View,
-  TouchableOpacity
+  View
 } from 'react-native'
 import { Camera, Permissions } from 'expo'
+import { kairosDetect } from '../../api/kairos'
 import FlipButton from '../buttons/CameraFlipBtn'
 import CameraButton from '../buttons/CustomBtn'
+import Loader from '../loader/Loader' 
 import styles from './style'
 
 class SnapShot extends React.Component {
@@ -14,9 +15,22 @@ class SnapShot extends React.Component {
     super()
     this.state = {
       hasCameraPermission: false,
-      type: Camera.Constants.Type.back
+      type: Camera.Constants.Type.back,
+      showSnapBtn: false,
+      isLoading: false
     }
+    this.camera = null
     this.onFlip = this.onFlip.bind(this)
+    this.faceDetector = this.faceDetector.bind(this)
+    this.takeSnap = this.takeSnap.bind(this)
+  }
+
+  faceDetector (facesData) {
+    if(facesData.faces.length > 0) {
+      this.setState({showSnapBtn: true})
+    } else {
+      this.setState({showSnapBtn: false})            
+    }
   }
 
   onFlip() {
@@ -27,22 +41,46 @@ class SnapShot extends React.Component {
     })
   }
 
+  async takeSnap () {
+    if(this.camera) {
+      let photo = await this.camera.takePictureAsync({base64: true})
+      this.setState({isLoading: true})
+
+      const handleResponse = (error, response) => {
+        if (error) {
+          console.warn(error.message)
+        }
+        console.warn(response.body)        
+      }
+
+      //kairosDetect(photo.base64, handleResponse)      
+    }
+  }
+
   async componentDidMount () {
     const { status } = await Permissions.askAsync(Permissions.CAMERA)
     this.setState({ hasCameraPermission: status === 'granted' })
   }
 
   render () {
-    const { hasCameraPermission, type } = this.state
+    const { hasCameraPermission, type, showSnapBtn, isLoading } = this.state
+
+    if (isLoading)
+      return (<Loader />)
+
+    if (!hasCameraPermission)
+      return (<Text>You Haven't Permissions</Text>)
+
     return (
-    <View style={styles.wrapper}>
-      <Camera style={styles.wrapper} type={type}>
-      <View style={styles.cameraContainer}>
-        <FlipButton onPress={this.onFlip} type={type} />
-        <CameraButton iconName="camera" />
-       </View>
-      </Camera>
-    </View>)
+      <View style={styles.wrapper}>
+        <Camera ref={ref => { this.camera = ref }} style={styles.wrapper} type={type} 
+          onFacesDetected={this.faceDetector} faceDetectionMode={Camera.Constants.FaceDetection.Mode.fast}>
+        <View style={styles.cameraContainer}>
+          <FlipButton onPress={this.onFlip} type={type} />
+          {showSnapBtn && <CameraButton iconName="camera" color="green" onPress={this.takeSnap} />}
+        </View>
+        </Camera>
+      </View>)
   }
 }
 
