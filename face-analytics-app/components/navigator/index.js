add66d8b466fe3e44a24c86b76f6f02bf90128d2
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-06-27 22:09:25 -0500

    Registered new view

--- face-analytics-app/components/navigator/index.js
+++ face-analytics-app/components/navigator/index.js
@@ -5,11 +5,14 @@ import {
 import Home from "../home/Home"
 import Welcome from "../welcome/Swiper"
 import SnapShot from "../camera/SnapShot"
+import Summary from "../summary/Summary"
+
 
 export default createStackNavigator({
-  Home: Home,
-  Welcome: Welcome,
-  SnapShot: SnapShot
+  Home,
+  Welcome,
+  Summary,
+  SnapShot
 }, {
   initialRouteName: 'SnapShot',
   headerMode: 'none'
