5d24d251086c552ec8b0f5db3a6b60cfd4e11c60
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-02 13:43:37 -0500

    Added card style and scroll view component, added image prop

--- face-analytics-app/components/summary/Summary.js
+++ face-analytics-app/components/summary/Summary.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {
   View,
+  ScrollView
 } from 'react-native'
 import { LinearGradient } from 'expo'
 import SummaryCard from './Card'
@@ -9,11 +10,13 @@ import styles from './style'
 class Summary extends React.Component {
   render () {
     const { navigation } = this.props
-    const images = this.props.navigation.getParam('images', [])
+    const images = navigation.getParam('images', [])
     const photo = navigation.getParam('photo', "")
     return (<View style={styles.summary}>
       <LinearGradient style={styles.wrapper} colors={styles.colors}>
-        <SummaryCard images={images} photo={photo} />
+        <ScrollView>
+          <SummaryCard images={images} photo={photo} />
+        </ScrollView>
       </LinearGradient>
     </View>)
   }
