7de1257d71d6bcc6bdbe89c3c7ca8d3a570c218f
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-02 13:42:34 -0500

    Test lottie

--- face-analytics-app/components/home/Home.js
+++ face-analytics-app/components/home/Home.js
@@ -4,12 +4,47 @@ import {
   View
 } from 'react-native'
 import styles from './style'
+import { DangerZone } from 'expo';
+const { Lottie } = DangerZone;
+import animationFile from '../../assets/loader1.json';
 
 class Home extends React.Component {
+
+  constructor () {
+    super()
+    this.state = { animation: animationFile }
+    this.playAnimation = this.playAnimation.bind(this)
+  }
+
+  componentDidMount () {
+    this.animation.reset();
+    this.animation.play();
+  }
+
+  playAnimation () {
+    console.log("hi")
+  }
+
   render () {
-    return (<View style={styles.home}>
-      <Text style={styles.text}>Home</Text>
-    </View>)
+    return (
+      <View style={styles.home}>
+        {this.state.animation &&
+        <Lottie
+          ref={animation => {
+            this.animation = animation;
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: 'black',
+          }}
+          source={this.state.animation}
+        />}
+      {/* <View style={styles.buttonContainer}>
+          <Button title="Restart Animation" onPress={this._playAnimation} />
+        </View> */}
+      </View>
+    )
   }
 }
 
