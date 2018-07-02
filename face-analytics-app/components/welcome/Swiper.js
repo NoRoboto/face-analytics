939008a76c151aafd0540d952b1ebce013f568e2
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-02 13:45:01 -0500

    Added styles, actions and text

--- face-analytics-app/components/welcome/Swiper.js
+++ face-analytics-app/components/welcome/Swiper.js
@@ -3,21 +3,42 @@ import {
   Text,
   View
 } from 'react-native';
-import styles from './style'
+import { Button } from 'react-native-elements';
+import styles from './style';
 import Swiper from 'react-native-swiper';
 
 export default class Example extends Component {
+
+  constructor () {
+    super()
+    this.launchCamera = this.launchCamera.bind(this)
+  }
+
+  launchCamera () {
+    const {navigation} = this.props
+    navigation.push('SnapShot')
+  }
+
   render() {
+    console.warn(`Last render at: ${new Date()}`)
     return (
       <Swiper style={styles.wrapper} showsButtons={false} loop={false} activeDotColor={styles.activeDotColor}>
         <View style={styles.slide1}>
-          <Text style={styles.text}>Hello Swiper</Text>
+        <Text style={styles.title}>Face Analytics</Text>
+          <Text style={styles.title}>Welcome User!</Text>
         </View>
         <View style={styles.slide2}>
-          <Text style={styles.text}>Beautiful</Text>
-        </View>
-        <View style={styles.slide3}>
-          <Text style={styles.text}>And simple</Text>
+          <Text style={styles.title}>Next, take a selfie!</Text>
+          <View style={styles.wrapper}>
+            <Button 
+              title='Launch Camera' 
+              buttonStyle={styles.button}
+              titleStyle={styles.buttonText}
+              onPress={this.launchCamera}
+              outline
+              large
+            />
+          </View>
         </View>
       </Swiper>
     );
