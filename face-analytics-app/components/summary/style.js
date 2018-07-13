5831d61c33abba118d1dd20ecc7fbe23eb0f5bd1
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-13 16:05:57 -0500

    Added styles to fix issues with scroll component

--- face-analytics-app/components/summary/style.js
+++ face-analytics-app/components/summary/style.js
@@ -11,9 +11,9 @@ export default {
     width: "100%",
     paddingTop: Constants.statusBarHeight + 20,
   },
+  scroll: {flexGrow: 1, minWidth:"100%"},
   cardTitle: {fontSize: 25},
   cardImage: {height: 150, width: 150},
-  cardWidth: {width: "90%"},
   centerX: {alignItems: 'center'},
   colors: ["#9b59b6", "#9b59b6", "#582d69"],
   text: classes.sectionText
