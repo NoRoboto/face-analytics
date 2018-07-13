7f638f299074528e72a1c489a562c3288715c5a8
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-13 16:01:34 -0500

    Added lottie animation

--- face-analytics-app/components/loader/Loader.js
+++ face-analytics-app/components/loader/Loader.js
@@ -1,13 +1,39 @@
 import React from 'react'
-import { ActivityIndicator, View, Text } from 'react-native'
+import {
+  View
+} from 'react-native'
 import styles from './style'
+import { DangerZone } from 'expo';
+const { Lottie } = DangerZone;
+import animationFile from '../../assets/loader1.json';
 
-const Loader = () => {
-  return (
-    <View style={styles.view}>
-      <ActivityIndicator size={60} color="#FFFFFF" />
-      <Text style={styles.text}>Loading :D</Text>
-    </View>)
+class Loader extends React.Component {
+
+  state = { animation: animationFile }  
+
+  componentDidMount () {
+    this.animation.reset();
+    this.animation.play();
+  }
+
+  render () {
+    return (
+      <View style={styles.home}>
+        {this.state.animation &&
+        <Lottie
+          ref={animation => {
+            this.animation = animation;
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: 'black',
+          }}
+          source={this.state.animation}
+        />}
+      </View>
+    )
+  }
 }
 
 export default Loader
