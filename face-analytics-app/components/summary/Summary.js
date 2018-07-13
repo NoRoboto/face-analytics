557db0af098aca31e56b357095a125e1798937a2
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-13 16:05:12 -0500

    Added props

--- face-analytics-app/components/summary/Summary.js
+++ face-analytics-app/components/summary/Summary.js
@@ -1,24 +1,19 @@
 import React from 'react'
 import {
-  View,
   ScrollView
 } from 'react-native'
-import { LinearGradient } from 'expo'
 import SummaryCard from './Card'
 import styles from './style'
 
 class Summary extends React.Component {
   render () {
-    const { navigation } = this.props
-    const images = navigation.getParam('images', [])
-    const photo = navigation.getParam('photo', "")
-    return (<View style={styles.summary}>
-      <LinearGradient style={styles.wrapper} colors={styles.colors}>
-        <ScrollView>
-          <SummaryCard images={images} photo={photo} />
-        </ScrollView>
-      </LinearGradient>
-    </View>)
+    const { images, photo } = this.props
+
+    return (
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <SummaryCard images={images} photo={photo} />
+      </ScrollView>
+    )
   }
 }
 
